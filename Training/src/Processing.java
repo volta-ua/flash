e7967b7a017c701c4992562270d18e4a2ab82206
volta-ua <olimpiaplace@gmail.com>
2019-01-06 12:30:30 +0200

    new

--- Training/src/Processing.java
+++ Training/src/Processing.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Processing")
 public class Processing extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
     /**
      * @see HttpServlet#HttpServlet()
      */
