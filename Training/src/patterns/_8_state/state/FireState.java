0a3516e77cbcacd6181a55908e5b55e558e1e7df
volta-ua <olimpiaplace@gmail.com>
2019-01-19 12:22:05 +0200

    again

--- Training/src/patterns/_8_state/state/FireState.java
+++ Training/src/patterns/_8_state/state/FireState.java
@@ -4,6 +4,7 @@ public class FireState implements TransformerState{
 
 	@Override
 	public void action() {
+		
 		System.out.println("Fire!!!");
 		
 	}
--- Training/src/patterns/_8_state/state/MoveState.java
+++ Training/src/patterns/_8_state/state/MoveState.java
@@ -4,6 +4,7 @@ public class MoveState implements TransformerState{
 
 	@Override
 	public void action() {
+		
 		System.out.println("Moving");
 		
 	}
