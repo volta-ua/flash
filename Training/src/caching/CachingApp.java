37f6f1386099cb84e53e326a33efc9b6378d1d5d
volta-ua <olimpiaplace@gmail.com>
2019-02-04 23:18:47 +0200

    rel 04-02 23-18

---
+++ Training/src/caching/CachingApp.java
@@ -0,0 +1,28 @@
+package caching;
+
+import javax.swing.JOptionPane;
+
+public class CachingApp {
+
+	public static void main(String[] args) {
+		double input=0, output;
+		
+		while (input==8888) {
+			input=Double.parseDouble(JOptionPane.showInputDialog("Enter the value"));
+			System.out.print(input+"\t");
+			output=doCalc(input);
+			System.out.println(output);
+		}
+		
+	}
+	
+	static double doCalc(double i) {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return Math.pow(i, 2);
+	}
+
+}
