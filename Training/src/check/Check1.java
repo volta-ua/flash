c54ea29302d5f2227c7be29d55cff433d7cd49dc
volta-ua <olimpiaplace@gmail.com>
2019-02-06 23:13:09 +0200

    06-02

--- Training/src/check/Check1.java
+++ Training/src/check/Check1.java
@@ -4,18 +4,23 @@ public class Check1 {
 
 	public static void main(String[] args) {
 		Base b=new Sub();
+		System.out.println(b.i);
 		
+		Sub s=new Sub();
+		System.out.println(((Base)s).i); //явне перевтілення можна робити для виклику змінних. З методом не вдасться
+		System.out.println(s.i);
 	}
-
 }
 
 class Base{
+	protected int i=5;
 	void doAction() {
 		System.out.println("Base");
 	}
 }
 
 class Sub extends Base{
+	protected int i=6;
 	void doAction() {
 		System.out.println("Sub");
 	}
