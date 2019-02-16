0e051b596dd2af36092508f0545cf2e5b16103a1
volta-ua <olimpiaplace@gmail.com>
2019-02-16 23:04:33 +0200

    16-02

---
+++ Training/src/check/Check9.java
@@ -0,0 +1,21 @@
+package check;
+
+public class Check9 {
+	
+	
+
+	public static void main(String[] args) {
+		AAAA a=new AAAA(5);
+
+	}
+
+}
+
+class AAAA{
+	int i;
+
+	AAAA(int i) {
+		this.i = i;
+	}
+	
+}
