5b532f9b3448d8cf5269e5913d5a9b8312af5201
volta-ua <olimpiaplace@gmail.com>
2019-02-17 23:37:13 +0200

    1702

---
+++ Training/src/check/Check13.java
@@ -0,0 +1,14 @@
+package check;
+
+public class Check13 {
+
+	public static void main(String[] args) {
+		System.out.println(getter(5));
+
+	}
+	
+	static int getter(int i) {
+		return (int)new Integer(i*i);
+	}
+
+}
