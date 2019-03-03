b34a5902a6e2cf35620a8675c07113ce3157fcff
volta-ua <olimpiaplace@gmail.com>
2019-03-03 23:55:37 +0200

    0303

--- Training/src/Json/Starter.java
+++ Training/src/Json/Starter.java
@@ -4,10 +4,8 @@ import com.google.gson.Gson;
 
 public class Starter {
 
-	public static void main(String[] args) {
-		
-		System.out.println(new Gson().toJson(new myClass()));	
-		
+	public static void main(String[] args) {		
+		System.out.println(new Gson().toJson(new myClass()));		
 	}
 	
 }
