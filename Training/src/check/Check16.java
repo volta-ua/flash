9619ca2b8cff4e90a85486816615129e4289eabe
volta-ua <olimpiaplace@gmail.com>
2019-03-24 01:31:50 +0200

    0324

---
+++ Training/src/check/Check16.java
@@ -0,0 +1,33 @@
+package check;
+
+public class Check16 {
+
+	public static void main(String[] args) {
+		
+		Check16 c=new Check16();
+		c.doSuperAction(new A1() {
+			@Override
+			String doF() {
+				System.out.println("doFFF");
+				return null;
+			}			
+		});
+
+	}
+	
+	void doSuperAction(Object ob) {
+		System.out.println("doSuperAction");
+	}
+
+}
+
+class A1{
+	A1(){
+		System.out.println("A");
+	}
+	String doF() {
+		System.out.println("doF");
+		return null;
+	}
+}
+
